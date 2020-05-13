650271ad751485b6e8a4409113bd41de393d1556
Ahajuu <56596060+Ahajuu@users.noreply.github.com>
2020-05-13 22:32:28 +0530

    Updates

---
+++ precache-manifest.398272cbfd7f38755d0fef4bb1653d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab0848ef40160fbf38c3af8989c1c47",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "0d4a6e8b0ae36d450492",
+    "url": "/Robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "6ebdf2781bec585b3c4b",
+    "url": "/Robofriends/static/css/main.911ec47d.chunk.css"
+  },
+  {
+    "revision": "0d4a6e8b0ae36d450492",
+    "url": "/Robofriends/static/js/2.663cd6b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.663cd6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebdf2781bec585b3c4b",
+    "url": "/Robofriends/static/js/main.f225984e.chunk.js"
+  },
+  {
+    "revision": "0ca19a742936d9a6b69e",
+    "url": "/Robofriends/static/js/runtime-main.2ef17c52.js"
+  }
+]);
